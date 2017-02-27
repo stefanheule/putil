cbc23ae73e2896d1ff706b3314f3bde04744481b
Stefan Heule <stefanheule@gmail.com>
2017-02-27 10:46:01 -0800

    work in progress

--- putil/color.py
+++ putil/color.py
@@ -13,17 +13,19 @@
 # COL_GREY="\033[38;5;244m"
 # COL_NO_COLOR="\033[0m"
 
+from builtins import str
+
 def grey(val):
-  return "\033[38;5;244m" + unicode(val) + "\033[0m"
+  return "\033[38;5;244m" + str(val) + "\033[0m"
 
 
 def red(val):
-  return "\033[38;5;1m" + unicode(val) + "\033[0m"
+  return "\033[38;5;1m" + str(val) + "\033[0m"
 
 
 def green(val):
-  return "\033[38;5;2m" + unicode(val) + "\033[0m"
+  return "\033[38;5;2m" + str(val) + "\033[0m"
 
 
 def yellow(val):
-  return "\033[38;5;3m" + unicode(val) + "\033[0m"
+  return "\033[38;5;3m" + str(val) + "\033[0m"
